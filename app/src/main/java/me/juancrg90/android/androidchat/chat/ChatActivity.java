3e586aebf75d3f23293f8b5f3fe7d0538c053214
Juan Carlos Ruiz <JuanCrg90@gmail.com>
2016-06-12 16:55:58 -0500

    Added Chat Layout

---
+++ app/src/main/java/me/juancrg90/android/androidchat/chat/ChatActivity.java
@@ -0,0 +1,15 @@
+package me.juancrg90.android.androidchat.chat;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import me.juancrg90.android.androidchat.R;
+
+public class ChatActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_chat);
+    }
+}
